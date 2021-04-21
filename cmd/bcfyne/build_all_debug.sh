6b9dd49d4f45bc69028d517750b0caf0375742e9
Stuart Scott <stuart.murray.scott@gmail.com>
2021-04-21 11:30:36 -0700

    Add icon parameter

--- cmd/bcfyne/build_all_debug.sh
+++ cmd/bcfyne/build_all_debug.sh
@@ -22,10 +22,10 @@ go fmt $GOPATH/src/aletheiaware.com/bcfynego/...
 go vet $GOPATH/src/aletheiaware.com/bcfynego/...
 go test $GOPATH/src/aletheiaware.com/bcfynego/...
 mkdir -p fyne-cross/logs
-(fyne-cross android -app-id com.aletheiaware.bc -debug -output BC_unaligned ./cmd/bcfyne >./fyne-cross/logs/android 2>&1 && cd $GOPATH/src/aletheiaware.com/bcfynego/fyne-cross/dist/android && ${ANDROID_HOME}/build-tools/28.0.3/zipalign -f 4 BC_unaligned.apk BC.apk) &
-fyne-cross darwin -app-id com.aletheiaware.bc -debug -output BC ./cmd/bcfyne >./fyne-cross/logs/darwin 2>&1 &
-fyne-cross linux -app-id com.aletheiaware.bc -debug -output bc ./cmd/bcfyne >./fyne-cross/logs/linux 2>&1 &
-#fyne-cross windows -app-id com.aletheiaware.bc -debug -output BC ./cmd/bcfyne >./fyne-cross/logs/windows 2>&1 &
+(fyne-cross android -app-id com.aletheiaware.bc -debug -icon ./cmd/bcfyne/Icon.png -output BC_unaligned ./cmd/bcfyne >./fyne-cross/logs/android 2>&1 && cd $GOPATH/src/aletheiaware.com/bcfynego/fyne-cross/dist/android && ${ANDROID_HOME}/build-tools/28.0.3/zipalign -f 4 BC_unaligned.apk BC.apk) &
+fyne-cross darwin -app-id com.aletheiaware.bc -debug -icon ./cmd/bcfyne/Icon.png -output BC ./cmd/bcfyne >./fyne-cross/logs/darwin 2>&1 &
+fyne-cross linux -app-id com.aletheiaware.bc -debug -icon ./cmd/bcfyne/Icon.png -output bc ./cmd/bcfyne >./fyne-cross/logs/linux 2>&1 &
+#fyne-cross windows -app-id com.aletheiaware.bc -debug -icon ./cmd/bcfyne/Icon.png -output BC ./cmd/bcfyne >./fyne-cross/logs/windows 2>&1 &
 for job in `jobs -p`
 do
     wait $job
